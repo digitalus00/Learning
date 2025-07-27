d151413a741a8ec4104a90b899bef193a1b25055
digitalus00 <digitalus002@gmail.com>
2025-07-27 10:49:47 +0545

    Update build.gradle.kts

--- WatchMoviesPkProvider/build.gradle.kts
+++ WatchMoviesPkProvider/build.gradle.kts
@@ -1,24 +1,25 @@
+// use an integer for version numbers
 version = 1
 
 cloudstream {
-    language = "hi"
+    // All of these properties are optional, you can safely remove any of them.
 
-    description = "Watch Online Movies in HD Print Quality Free Download,Watch Full Movies Online Bollywood Movies Download Latest Hollywood Movies in DVD Print Quality Free"
-
-    authors = listOf("salman731")
+    language = "en"
+    authors = listOf("digitalus")
 
     /**
-     * Status int as the following:
+     * Status int as one of the following:
      * 0: Down
      * 1: Ok
      * 2: Slow
-     * 3: Beta only
-     * */
-    status = 1 // will be 3 if unspecified
-    tvTypes = listOf(
-        "TvSeries",
-        "Movie",
-    )
+     * 3: Beta-only
+     **/
+    status = 1 // Will be 3 if unspecified
 
-    iconUrl = "https://www.watch-movies.com.pk/wp-content/uploads/2022/05/logo.png"
+    tvTypes = listOf(
+        "NSFW"
+    )
+    iconUrl = "https://www.google.com/s2/favicons?domain=http://example.com&sz=256"
+
+    isCrossPlatform = true
 }
